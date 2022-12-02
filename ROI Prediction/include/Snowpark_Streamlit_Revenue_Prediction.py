5087d2ec904c5a2c9923954a4792f1bb9cc07638
vyshnavipendru <pendru.v@gmail.com>
2022-12-02 10:59:08 -0500

    remove credentials

--- ROI Prediction/include/Snowpark_Streamlit_Revenue_Prediction.py
+++ ROI Prediction/include/Snowpark_Streamlit_Revenue_Prediction.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import col
 import streamlit as st
 
-# APP_ICON_URL = "https://i.imgur.com/dBDOHH3.png"
+
 import sys
 sys.path.append('..')
 
@@ -25,10 +25,9 @@ def load_data():
     df_last_months_allocations = historical_data.filter(col("MONTH") == "June").to_pandas()
     return historical_data.to_pandas(), df_last_six_months_allocations, df_last_six_months_roi, df_last_months_allocations
 
-# st.set_page_config("SportsCo Ad Spend Optimizer", APP_ICON_URL, "centered")
+
 st.write("<style>[data-testid='stMetricLabel'] {min-height: 0.5rem !important}</style>", unsafe_allow_html=True)
-# st.image(APP_ICON_URL, width=80)
-st.title("SportsCo Ad Spend Optimizer")
+st.title("Return Over Investment for the budget spent on Adverstisement")
 session = create_session()
 historical_data, df_last_six_months_allocations, df_last_six_months_roi, df_last_months_allocations = load_data()
 
@@ -52,42 +51,7 @@ def predict(budgets):
 
 predicted_roi, change = predict(budgets)
 st.metric("", f"$ {predicted_roi:.2f} million", f"{change:.1f} % vs last month")
-# months = ["January", "February", "March", "April", "May", "June", "July"]
-# july = pd.DataFrame({"MONTH": ["July", "July", "July", "July"], "CHANNEL": ["SEARCHENGINE", "SOCIALMEDIA", "VIDEO", "EMAIL"], "BUDGET": budgets, "ROI": [predicted_roi] * 4})
-# chart_data = historical_data.append(july).reset_index(drop=True)
-# chart_data = chart_data.replace(["SEARCHENGINE", "EMAIL", "SOCIALMEDIA", "VIDEO"], ["Search engine", "Email", "Social media", "Video"])
-# base = alt.Chart(chart_data).encode(alt.X("MONTH", sort=months, title=None))
-# bars = base.mark_bar().encode(
-#     y=alt.Y("BUDGET", title="Budget", scale=alt.Scale(domain=[0, 400])),
-#     color=alt.Color("CHANNEL", legend=alt.Legend(orient="top", title=" ")),
-#     opacity=alt.condition(alt.datum.MONTH == "July", alt.value(1), alt.value(0.3)),
-# )
-# lines = base.mark_line(size=3).encode(
-#     y=alt.Y("ROI", title="Revenue", scale=alt.Scale(domain=[0, 25])),
-#     color=alt.value("#808495"),
-#     tooltip=["ROI"],
-# )
-# points = base.mark_point(strokeWidth=3).encode(
-#     y=alt.Y("ROI"),
-#     stroke=alt.value("#808495"),
-#     fill=alt.value("white"),
-#     size=alt.condition(alt.datum.MONTH == "July", alt.value(300), alt.value(70)),
-# )
-# chart = alt.layer(bars, lines + points).resolve_scale(y="independent")
-# chart = chart.configure_view(strokeWidth=0).configure_axisY(domain=False).configure_axis(labelColor="#808495", tickColor="#e6eaf1", gridColor="#e6eaf1", domainColor="#e6eaf1", titleFontWeight=600, titlePadding=10, labelPadding=5, labelFontSize=14).configure_range(category=["#FFE08E", "#03C0F2", "#FFAAAB", "#995EFF"])
-# st.altair_chart(chart, use_container_width=True)
 
-# submitted = st.button("❄️ Save to Snowflake")
-# if submitted:
-#     with st.spinner("Making snowflakes..."):
-#         df = pd.DataFrame({"MONTH": ["July"], "SEARCHENGINE": [budgets[0]], "SOCIALMEDIA": [budgets[1]], "VIDEO": [budgets[2]], "EMAIL": [budgets[3]], "ROI": [predicted_roi]})
-#         session.write_pandas(df, "BUDGET_ALLOCATIONS_AND_ROI")
-#         # session.sql("INSERT INTO BUDGET_ALLOCATIONS_AND_ROI VALUES ('July'" + "," + str(budgets[0]) + "," + str(budgets[1]) + "," + str(budgets[2]) + "," + str(budgets[3]) + "," + str(predicted_roi) + ")").collect()
-#         st.success("✅ Successfully wrote budgets & prediction to your Snowflake account!")
-#         st.snow()
-
-        
-        
 
         
 # running streamlit
